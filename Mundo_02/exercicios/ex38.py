81f4b2fd04605205f4a5b695956f05b930cf49cf
WFCArthur <tutuco040@gmail.com>
2026-03-26 00:36:33 -0300

    update

--- Mundo_02/exercicios/ex38.py
+++ Mundo_02/exercicios/ex38.py
@@ -5,3 +5,13 @@
 # O segundo valor é maior
 
 # Não existe valor maior, os dois são iguais
+
+num0 = float(input('Digite um Número: '))
+num1 = float(input('Digite um Número: '))
+
+if num0 > num1:
+    print(f'O primeiro valor é maior {num0}!')
+elif num1 > num0:
+    print(f'O segundo valor é maior! {num1}')
+else:
+    print('Não existe valor maior, os dois são iguais!')
--- Mundo_02/exercicios/ex39.py
+++ Mundo_02/exercicios/ex39.py
@@ -1,4 +1,4 @@
-# Faça um  programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
+# Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade:
 
 # Se ele ainda vai se alistar ao serviço militar.
 
@@ -7,3 +7,31 @@
 # se já passou do tempo do alistamento.
 
 # Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+from time import sleep
+print('-=-'*13)
+print('quantos anos você vai fazer esse ano?')
+print('-=-'*13)
+
+idade = int(input('Digite a sua idade: '))
+
+print('CARREGANDO....')
+sleep(2)
+
+ano = 2026 - idade
+
+if idade <= 17:
+    print(f'O ano que você nasceu é {ano}')
+    print('Você ainda não pode se alistar!')
+    print(f'Por que você não tem idade suficiente! {idade} anos')
+elif idade == 18:
+    print(f'O ano que você nasceu é {ano}')
+    print(f'Esse ano você faz {idade} anos, você já pode se alistar!')
+elif idade > 19:
+    print(f'O ano que você nasceu é {ano}')
+    print(f'Já passou do tempo de se alistar! Pois você já tem {idade} anos')
+else:
+    print('[ERRO]')
+
+print('-'*20)
+
+
